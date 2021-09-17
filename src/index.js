65d4462a9278e7fde4d6ee2d3ee36bdb98212d5a
ChovyDoinb <binhdinhqt137@gmail.com>
2021-09-17 15:37:26 +0700

    refactot code: routes, controlle

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ const morgan = require("morgan");
 const hbs = require("express-handlebars");
 const port = 3000;
 
+//Có file index cũng được không có thì nó sẽ tự tìm
+// const route = require("./routes/index")
+const route = require("./routes/index");
+
 //static path
 app.use(express.static(path.join(__dirname, "public")));
 
 //HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 //Template engine
 app.engine(
@@ -20,12 +24,15 @@ app.engine(
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   })
+// );
+// app.use(express.json());
 
-console.log("PATH", __dirname);
-
-app.get("/", function (req, res) {
-  res.render("home");
-});
+//Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
