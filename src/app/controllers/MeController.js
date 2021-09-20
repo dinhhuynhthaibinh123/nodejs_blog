9e118fdca9f54250e13d13f8cfdb1ce01346956c
ChovyDoinb <binhdinhqt137@gmail.com>
2021-09-20 23:59:19 +0700

    trash courses page

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -6,15 +6,16 @@ const { multipleMongooseToObject } = require("../../utils/mongoose");
 class MeController {
   //[GET] /me/stored/courses
   storedCourses(req, res, next) {
-    Course.find({})
-      .then((courses) =>
+    Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+      .then(([courses, count]) =>
         res.render("me/stored-courses", {
+          count,
           courses: multipleMongooseToObject(courses),
         })
       )
       .catch(next);
   }
-   //[GET] /me/trash/courses
+  //[GET] /me/trash/courses
   trashCourses(req, res, next) {
     //Hàm này lấy dữ liệu mà deleted = true
     Course.findDeleted({})
